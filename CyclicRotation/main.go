4cd17cb237b2625b99b3c3c638b877f6836f7b81
Phanor Coll <phanorcoll@gmail.com>
2017-10-13 20:39:43 -0400

    Cyclic Rotation
    initial structure

---
+++ CyclicRotation/main.go
@@ -0,0 +1,20 @@
+/*
+Each element the slice will be shifted to the right by N indexes
+For example, given array
+A = [3, 8, 9, 7, 6] and K = 3,
+the function should return [9, 7, 6, 3, 8].
+*/
+package main
+
+import "fmt"
+
+//Solution Rotates A K indexes to the right
+func Solution(A []int, K int) []int {
+
+	return A
+}
+
+func main() {
+	t := []int{1, 2, 3}
+	fmt.Println(Solution(t, 3))
+}
