dfed7afc85f09df9551f813bad5b626d5e3c1739
Phanor Coll <phanorcoll@gmail.com>
2017-10-19 16:06:12 -0400

    FIXED BUG
    added support for K >= N

--- CyclicRotation/main.go
+++ CyclicRotation/main.go
@@ -13,10 +13,12 @@ func Solution(A []int, K int) []int {
 
 	pos := 0
 	tmp := make([]int, len(A))
+	a := 0
 	for i := range A {
 		pos = i + K
 		if pos >= len(A) {
-			tmp[pos-len(A)] = A[i]
+			a = (pos % len(A))
+			tmp[a] = A[i]
 		} else {
 			tmp[pos] = A[i]
 		}
@@ -26,7 +28,7 @@ func Solution(A []int, K int) []int {
 
 func main() {
 	t := []int{3, 8, 9, 7, 6}
-	p := 3
+	p := 13
 	fmt.Printf("Original %x \npositions to the right K=%d \nFinal Result %x\n", t, p, Solution(t, p))
 	//fmt.Println(Solution(t, p))
 }
